4e84b04c880065dbd66c774d9877c3dfc1baf975
Shivee Yadav <shiveeyadav089@gmail.com>
2023-09-15 23:53:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = (product) => {
+    const updatedCart = cartItems.filter((item) => item.id !== product.id);
+    setCartItems(updatedCart);
+  };
+
+  const subtotal = cartItems.reduce((acc, item) => acc + item.price, 0);
+  const discount = 0; // Implement discount logic here
+  const total = subtotal - discount;
+
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<ProductList category="all" onAddToCart={addToCart} />} />
+        <Route path="/cart" element={<Cart cartItems={cartItems} onRemoveFromCart={removeFromCart} />} />
+        <Route path="/checkout" element={<Checkout cartItems={cartItems} subtotal={subtotal} discount={discount} total={total} />} />
+      </Routes>
     </div>
+  
   );
 }
 
